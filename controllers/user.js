f06f71f400d4cf65c48772d735ab0c29bb727c8a
David Pizarro <davidpizarrofrick@gmail.com>
2020-09-08 12:33:56 +0200

    Getting active users

--- controllers/user.js
+++ controllers/user.js
@@ -89,8 +89,22 @@ function getUsers(req, res) {
   });
 }
 
+function getUsersActive(req, res) {
+  const query = req.query;
+  User.find({ active: query.active }).then((users) => {
+    if (!users) {
+      res
+        .status(404)
+        .send({ message: "No se ha encontrado ningún usuario activo" });
+    } else {
+      res.status(200).send({ users });
+    }
+  });
+}
+
 module.exports = {
   signUp,
   signIn,
   getUsers,
+  getUsersActive,
 };
