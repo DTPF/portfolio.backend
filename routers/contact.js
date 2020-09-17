494d909a6b3ca379ab07212b0f8e380fcaad53aa
David Pizarro <davidpizarrofrick@gmail.com>
2020-09-17 16:37:57 +0200

    Fixing contact route

--- routers/contact.js
+++ routers/contact.js
@@ -3,6 +3,6 @@ const ContactController = require("../controllers/contact");
 
 const api = express.Router();
 
-api.post("/contact-me/:email", ContactController.contactMe);
+api.post("/contact-me/", ContactController.contactMe);
 
 module.exports = api;
